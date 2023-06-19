859ab9f1af6f1a76b331403912a6153362efaf0c
Carol Wargo <Carolwargo@gmail.com>
2023-06-19 13:34:03 -0400

    update thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,54 +1,41 @@
-const { Schema, model } = require('mongoose');
-
-const mongoose = require('mongoose');
-const reactionSchema = require('./ReactionSchema');
-
-//set up VIRTUAL REACTION 
-
-const reactionSchema = new mongoose.Schema({
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: timestamp => dateFormat(timestamp)
-  }
-});
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction");
+//set up VIRTUAL REACTION
 
 // THOUGHT schema
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 280
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: timestamp => dateFormat(timestamp)
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  reactions: [reactionSchema]
-});
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 // VIRTUAL field `reactionCount`
-thoughtSchema.virtual('reactionCount').get(function() {
+thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
 // CREATE the Thought model
-const Thought = mongoose.model('Thought', thoughtSchema);
+const Thought = mongoose.model("Thought", thoughtSchema);
 
 module.exports = Thought;
-
