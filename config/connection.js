93824774d6434f46f3ccf6c04aaaa88ad4e658a7
Carol Wargo <Carolwargo@gmail.com>
2023-06-19 11:08:55 -0400

    update notes

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// CREATE & NAME --- then add name to mongoose.connect
 // WRAP Mongoose around local connection to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/myfirstmongooseapp');
 
