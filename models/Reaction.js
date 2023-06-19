2b12f659de54ab56cdc24588e40774a4000ec420
Carol Wargo <Carolwargo@gmail.com>
2023-06-19 13:26:53 -0400

    updated reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,25 +1,43 @@
 // REACTION schema t THOUGHTS model
-const mongoose = require('mongoose');
+const { Schema, Types } = require("mongoose");
 
-const reactionSchema = new mongoose.Schema({
-  reactionId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: () => new mongoose.Types.ObjectId()
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+
+      // GENERATES a new ObjectId by default for each reaction
+      default: () => new mongoose.Types.ObjectId(),
+    },
+
+    // LIMITS reaction body  to 280 characters
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+
+    // REQUIRES username for reaction
+    username: {
+      type: String,
+      required: true,
+    },
+
+    // SETS default value of createdAt field to the current timestamp
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
   },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: timestamp => dateFormat(timestamp)
+
+  {
+    // GETTER is applied to createdAt to format date using dateFormat function
+    toJSON: {
+      getters: true,
+    },
+    id: false,
   }
-});
+);
 
 module.exports = reactionSchema;
