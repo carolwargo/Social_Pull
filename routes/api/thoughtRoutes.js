35e19203ce42f99d6c1e190e403433b826877389
Carol Wargo <Carolwargo@gmail.com>
2023-06-23 22:46:11 -0400

    update function names

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -15,13 +15,13 @@ router.route("/").get(getAllThoughts).post(createThought);
 // api/thoughts/:thoughtId
 router
   .route("/:thoughtId")
-  .get(getThought)
+  .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
 router
   .route("/:thoughtId/reactions/:reactionId")
-  .post(addThoughtReaction)
-  .delete(removeThoughtReaction);
+  .post(createReaction)
+  .delete(deleteReaction);
 
 module.exports = router;
